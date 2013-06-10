cc288bda704a7a7c0f5409f2113d30a84ade939a
SUGYO <sugyo@pb.jp.nec.com>
2013-06-10 11:29:26 +0900

    add copyright

--- backend_database/create_agent_table.sql
+++ backend_database/create_agent_table.sql
@@ -1,3 +1,7 @@
+--
+-- Copyright (C) 2012-2013 NEC Corporation
+--
+
 use vnet;
 
 create table agents (
--- backend_database/create_database.sql
+++ backend_database/create_database.sql
@@ -1,2 +1,6 @@
+--
+-- Copyright (C) 2012-2013 NEC Corporation
+--
+
 drop database if exists vnet;
 create database vnet;
